52023951cc69fd585e6826c3944ab4d67cf92b78
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2020-06-03 19:35:20 -0300

    Last changes

--- api/notifications.php
+++ api/notifications.php
@@ -4,4 +4,11 @@
  *Aquí se deberá recibir y procesar la notificación IPN
  */
 
+ /**
+ * Se recomienda usar una variable db como "base de datos" para guardar 
+ * la relación entre una órden y su estado actual.
+ * 
+ * (Aclaración: En un entorno productivo se requiere almacenar esta información de una manera más confiable)
+ */
+
 ?>
